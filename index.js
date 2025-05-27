819ee7739a7f6652c65b85dc1cbed6a275db91e4
Avik Roy <somavik99@gmail.com>
2025-05-27 12:53:57 +0530

    main

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // console.log("Welcome to the Node.js application!");
-import express from "express";
-import http from "http";
-import * as socketIo from "socket.io"
-
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+const http = require("http");
 
 
 const app = express()
 const server = http.createServer(app)
-const io =  new socketIo.Server(server)
+const io =  new Server(server)
 const PORT  = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
 app.set(express.static(path.join(__dirname,"public")))
 
+io.on("connection",function(socket){
+    console.log("connected")
+})
+
+
 app.get("/",function(req,res){
-res.send("hey")
+res.render("Index")
 })
 
 server.listen(PORT)
