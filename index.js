3a1372658c7c764ff23f081fd22f204baf9a078c
Avik Roy <somavik99@gmail.com>
2025-05-30 13:58:17 +0530

    main

--- index.js
+++ index.js
@@ -5,25 +5,33 @@ const { Server } = require("socket.io");
 const http = require("http");
 // const ejs = require("ejs")
 
-const app = express()
-const server = http.createServer(app)
-const io =  new Server(server)
-const PORT  = process.env.PORT || 3000
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs")
-// app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname,"public")))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection",function(socket){
-    console.log("connected")
-})
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  console.log("connected");
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", { id: socket.id });
+  });
+});
 
+app.get("/", function (req, res) {
+  res.render("Document", {
+    title: "criminal alert application",
+    message: "Hello from EJS!",
+  });
+});
 
-app.get("/",function(req,res){
-res.render('Document', { title: 'criminal alert application', message: 'Hello from EJS!' });
-})
-
-server.listen(PORT, function(){
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-})
+server.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,15 @@
 const socket = io();
+const markers = {};
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
-    function (position) {
+    (position) => {
       const { latitude, longitude } = position.coords;
       socket.emit("send-location", {
         latitude,
         longitude,
       });
     },
-    function (err) {
+    (err) => {
       console.error(err);
     },
     {
@@ -19,4 +20,27 @@ if (navigator.geolocation) {
   );
 }
 
-L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+socket.on("receive-location", (data) => {
+  const { latitude, longitude, id } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected",(id)=>{
+if(markers[id]){
+    map.removeLayers(markers[id])
+    delete markers[id];
+}
+})
