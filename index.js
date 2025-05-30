9dc0a07ec2beaad2c0092c1987090e71a55a2b61
Avik Roy <somavik99@gmail.com>
2025-05-30 11:13:56 +0530

    dependencies and ejs file update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
 const http = require("http");
-
+// const ejs = require("ejs")
 
 const app = express()
 const server = http.createServer(app)
@@ -11,7 +11,8 @@ const io =  new Server(server)
 const PORT  = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
-app.set(express.static(path.join(__dirname,"public")))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"public")))
 
 io.on("connection",function(socket){
     console.log("connected")
@@ -19,7 +20,10 @@ io.on("connection",function(socket){
 
 
 app.get("/",function(req,res){
-res.render("Index")
+res.render('Document', { title: 'criminal alert application', message: 'Hello from EJS!' });
 })
 
-server.listen(PORT)
+server.listen(PORT, function(){
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+})
